f7f154ba314a9dac6ac3c584280bc97c18e1447d
Stephan Hohne <S2H.Mobile@gmail.com>
2019-01-19 15:33:19 +0100

    feat: Store contact messages in a vector

--- topological_swarm/src/subcontact.cpp
+++ topological_swarm/src/subcontact.cpp
@@ -1,12 +1,19 @@
 // This ROS node subscribes to the bumper sensor
 // and stores contact events in a file.
+#include <vector>
+#include <iostream>
 #include <ros/ros.h>
 #include <gazebo_msgs/ContactsState.h>
 #include <gazebo_msgs/ContactState.h>
 
+// list of contact events
+std::vector<gazebo_msgs::ContactState> contactStates;
+
+// handle a message from the bumper sensor
 void contactMessageReceived(const gazebo_msgs::ContactsState &msg) {
   if (!(msg.states.empty())) {
-    ROS_INFO_STREAM("contact message received, is empty " << msg.states[0] << "\n");
+    gazebo_msgs::ContactState contactState = msg.states[0];
+    contactStates.assign(contactStates.size() + 1, contactState);
   }
 }
 
@@ -15,4 +22,7 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe("bumper_sensor_state", 1000, &contactMessageReceived);
   ros::spin();
+
+  // TODO store the list of contact events
+  std::cout << "Contact states vector contains " << contactStates.size() <<" elements.\n";
 }
