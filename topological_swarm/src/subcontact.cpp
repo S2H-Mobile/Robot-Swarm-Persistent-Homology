622195af1afb68a33991eb09809544a4d3a0c32a
Stephan Hohne <S2H.Mobile@gmail.com>
2019-01-20 01:49:31 +0100

    feat: Add ContactEvent class to subscriber node

--- topological_swarm/src/subcontact.cpp
+++ topological_swarm/src/subcontact.cpp
@@ -2,19 +2,68 @@
 // and stores contact events in a file.
 #include <vector>
 #include <iostream>
+#include <string>
 #include <ros/ros.h>
+#include <ros/time.h>
 #include <gazebo_msgs/ContactsState.h>
 #include <gazebo_msgs/ContactState.h>
+#include <geometry_msgs/Vector3.h>
+
+class ContactEvent {
+
+  public:
+    void setContactTime(ros::Time time);
+    void setContactPosition(geometry_msgs::Vector3 position);
+    void print();
+
+  private:
+    ros::Time contactTime;
+    geometry_msgs::Vector3 contactPosition;
+
+};
+
+void ContactEvent::setContactTime(ros::Time time) {
+  contactTime = time;
+}
+
+void ContactEvent::setContactPosition(geometry_msgs::Vector3 position) {
+  contactPosition = position;
+}
+
+void ContactEvent::print() {
+  std::cout << contactTime << "\n" << contactPosition;
+}
 
 // list of contact events
-std::vector<gazebo_msgs::ContactState> contactStates;
+std::vector<ContactEvent> contactEvents;
 
 // handle a message from the bumper sensor
 void contactMessageReceived(const gazebo_msgs::ContactsState &msg) {
   if (!(msg.states.empty())) {
-    gazebo_msgs::ContactState contactState = msg.states[0];
-    contactStates.assign(contactStates.size() + 1, contactState);
+
+    // read the data from the message
+    const gazebo_msgs::ContactState contactState = msg.states[0];
+    const std::string info = contactState.info;
+    const geometry_msgs::Vector3 position = contactState.contact_positions[0];
+
+    // construct a contact event
+    ContactEvent event;
+    event.setContactTime(ros::Time::now());
+    event.setContactPosition(position);
+
+    // append the event to the global vector
+    contactEvents.push_back(event);
   }
+}
+
+void writeContactsToFile() {
+
+  // generate file name
+  const std::string name = ros::this_node::getName();
+
+  std::cout << "Contact events vector for " << name << " contains " << contactEvents.size() <<" elements.\n";
+
+  // TODO store the list of contact events
 }
 
 int main(int argc, char **argv) {
@@ -22,7 +71,5 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe("bumper_sensor_state", 1000, &contactMessageReceived);
   ros::spin();
-
-  // TODO store the list of contact events
-  std::cout << "Contact states vector contains " << contactStates.size() <<" elements.\n";
+  writeContactsToFile();
 }
