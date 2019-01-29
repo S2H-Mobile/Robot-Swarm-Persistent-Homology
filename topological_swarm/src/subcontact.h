af6e46ccaef861363ea87c58cc07751fd020a587
Stephan Hohne <S2H.Mobile@gmail.com>
2019-01-29 01:11:12 +0100

    feat: Add geometry data to ContactEvent class

--- topological_swarm/src/subcontact.h
+++ topological_swarm/src/subcontact.h
@@ -14,13 +14,19 @@ class ContactEvent {
   public:
     void setContactTime(ros::Time time);
     void setContactPosition(geometry_msgs::Vector3 position);
+    void setMyGeometry(std::string input);
+    void setOtherGeometry(std::string input);
     ros::Time getContactTime();
     geometry_msgs::Vector3 getContactPosition();
+    std::string getMyGeometry();
+    std::string getOtherGeometry();
     void print();
 
   private:
     ros::Time contactTime;
     geometry_msgs::Vector3 contactPosition;
+    std::string myGeometry;
+    std::string otherGeometry;
 
 };
 
@@ -32,12 +38,28 @@ void ContactEvent::setContactPosition(geometry_msgs::Vector3 position) {
   contactPosition = position;
 }
 
+void ContactEvent::setMyGeometry(std::string input) {
+  myGeometry = input;
+}
+
+void ContactEvent::setOtherGeometry(std::string input) {
+  otherGeometry = input;
+}
+
 ros::Time ContactEvent::getContactTime() {
   return contactTime;
 }
 
 geometry_msgs::Vector3 ContactEvent::getContactPosition() {
   return contactPosition;
+}
+
+std::string ContactEvent::getMyGeometry() {
+  return myGeometry;
+}
+
+std::string ContactEvent::getOtherGeometry() {
+  return otherGeometry;
 }
 
 void ContactEvent::print() {
