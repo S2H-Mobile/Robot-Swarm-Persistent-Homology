1be55a20fd50742a872688dc73f97edcceeaa641
Stephan Hohne <S2H.Mobile@gmail.com>
2019-03-06 12:48:16 +0100

    feat: Set distinct random seed for each agent
    Use node name of agent to initialize the seed for random movement.

--- topological_swarm/src/pubvel.cpp
+++ topological_swarm/src/pubvel.cpp
@@ -1,12 +1,18 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <stdlib.h>
+#include <boost/functional/hash.hpp>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "publish_velocity");
   ros::NodeHandle nh;
   ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
-  srand(time(0));
+
+  // initialize rand with current time and node name
+  std::string name = ros::this_node::getName();
+  const int seed = time(NULL) + boost::hash<std::string>()(name);
+  srand(seed);
+
   ros::Rate rate(2);
 
   while(ros::ok()) {
